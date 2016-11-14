6644d6591cc2223914748806e5b90e88bd4a19f7
Jorge Chávez Prebe 10 <chd.jl@hotmail.com>
2016-11-13 23:10:19 -0600

    Front renderizado

--- error.php
+++ error.php
@@ -2,22 +2,45 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>La Banca - Confirmación de eliminación de cuenta</title>
-  <link rel="stylesheet" href="css/materialize.min.css">
-  <link rel="stylesheet" href="css/style.css">
+  <title>Fatal error</title>
+  <link rel="stylesheet" href="materialize/css/materialize.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans|Pacifico" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=News+Cycle|PT+Sans|Pacifico" rel="stylesheet">
 </head>
-<body>
+ <body bgcolor="#D96C6C">
+
+ <nav>
+    <div class="nav-wrapper black   white-text">
+      <a href="index.php" style="font-family: 'Raleway', sans-serif"  class="brand-logo left"><img class="responsive-img" id="logo" src="img/protlog.png" width="65"/><p style="margin-bottom: 10px;margin-left:90px;margin-top: -80px">¡ERROR!</p></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>
+          <div class="row">
+            <form class="col s12 " action="login.jsp" method="POST">
+              <div class="row"> 
+
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.min.js"></script>
   <div class="row">
-    <div class="col l12 s12">
-      <div class="card grey darken-2">
-        <div class="card-content white-text">
-          <span class="card-title">Operación fallida</span>
-          <p>Error en los datos ingresados.</p>
+    <div class="col l6 offset-l3">
+      <div class="card black darken-2">
+        <div class="card-content white-text" style="text-align: center">
+          <span class="card-title center ">¡Usuario o contraseña inválidos!</span><br>
+          <span>Regresa para intentar de nuevo</span>
         </div>
         <div class="card-action center">
-          <a href="index.php" class="waves-effect waves-light btn">Regresar a la página de inicio.</a>
+          <a href="index.php" class="waves-effect waves-light btn #e57373 red lighten-2">Regresar a la página principal</a>
         </div>
       </div>
     </div>
--- success.php
+++ success.php
@@ -2,44 +2,34 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>BANCO PROFILIA</title>
-  <link rel="stylesheet" href="css/materialize.min.css">
-  <link rel="icon" type="image/gif" href="img/logo.gif"/>
+  <title>Registro exitoso</title>
+  <link rel="stylesheet" href="materialize/css/materialize.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans|Pacifico" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=News+Cycle|PT+Sans|Pacifico" rel="stylesheet">
 </head>
-<style type="text/css">
-    li.active a{
-      background-color: #aaa;
-      font-weight: bold;
-      color: black;
-    }
-    li:hover a{
-      color: #999;
-    }
-    </style>
  <body bgcolor="#4F4F4F">
-    <div class="navbar-fixed" style="font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif">
-      <nav>
-        <div class="nav-wrapper black white-text">
-          <a href="index.php" class="brand-logo"><img src="img/logo_icon.png" style="height: 64%; width: 64%"></a>
-          <a href="index.php" class="brand-logo" style="position: relative; margin-left: 75px">Banco Profilia</a>
-          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="historia_pobres.php">Historia</a></li>
-            <li><a href="servicios_pobres.php">Servicios</a></li>
-            <li><a href="clientes_pobres.php">Clientes</a></li>
-            <li><a href="comentarios_pobres.php">Comentarios</a></li>
-            <li><a href="about_pobres.php">About Us</a></li>
-          </ul>
-          <ul class="side-nav" id="mobile-demo">
-            <li><a href="historia_pobres.php">Historia</a></li>
-            <li><a href="servicios_pobres.php">Servicios</a></li>
-            <li><a href="clientes_pobres.php">Clientes</a></li>
-            <li><a href="comentarios_pobres.php">Comentarios</a></li>
-            <li><a href="about_pobres.php">About Us</a></li>
-          </ul>
+
+ <nav>
+    <div class="nav-wrapper black   white-text">
+      <a href="index.php" style="font-family: 'Raleway', sans-serif"  class="brand-logo left"><img class="responsive-img" id="logo" src="img/protlog.png" width="65"/><p style="margin-bottom: 10px;margin-left:90px;margin-top: -80px">Cursos intersemestrales</p></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>
+          <div class="row">
+            <form class="col s12 " action="login.jsp" method="POST">
+              <div class="row"> 
+
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.min.js"></script>
   <div class="row">
